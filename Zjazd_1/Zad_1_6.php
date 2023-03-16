4fffed5765334cb83e0713cad8c4c49eaa1c4273
Andrzej Ebertowski <andrzeje@macbook-pro-andrzej.home>
2023-03-16 17:47:47 +0100

    Merge remote-tracking branch 'origin/main'

--- Zjazd_1/Zad_1_6.php
+++ Zjazd_1/Zad_1_6.php
@@ -1,11 +1,37 @@
-<?php
-$a = readline("Podaj długość boku a: ");
-$b = readline("Podaj długość boku b: ");
-$c = readline("Podaj długość boku c: ");
+<html lang="pl">
+<head>
+    <title>Zad_1_6</title>
+</head>
+<body>
+<form method="post">
 
-if ($a + $b > $c && $a + $c > $b && $b + $c > $a) {
-    echo "Można zbudować trójkąt.\n";
-} else {
-    echo "BŁĄD: Nie można zbudować trójkąta z podanych boków.\n";
+    <label for="a">Podaj długość a:
+        <input type="number" name="a" required>
+    </label><br>
+    <label for="a">Podaj długość b:
+        <input type="number" name="b" required>
+    </label><br>
+    <label for="a">Podaj długość c:
+        <input type="number" name="c" required>
+    </label><br>
+    <input type="submit" name="submit" value="wynik">
+</form>
+
+<?php
+if (isset($_POST['submit'])) {
+    $a = $_POST['a'];
+    $b = $_POST['b'];
+    $c = $_POST['c'];
+
+    if ($a + $b > $c && $a + $c > $b && $b + $c > $c) {
+        echo "Można zbudować trójkąt. \naaaaaaa";
+    }else {
+        echo "Nie mozna zbudować trójkąta z podanych boków.";
+    }
+
 }
 ?>
+
+</body>
+</html>
+
